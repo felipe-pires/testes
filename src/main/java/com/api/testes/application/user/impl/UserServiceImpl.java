73532ae23ee554f625dcf7117098e95771ecca30
Felipe dos Santos <felipedossantospires01@gmail.com>
2023-01-19 21:56:13 -0300

    fix: add logs

--- src/main/java/com/api/testes/application/user/impl/UserServiceImpl.java
+++ src/main/java/com/api/testes/application/user/impl/UserServiceImpl.java
@@ -9,10 +9,14 @@ import org.springframework.stereotype.Service;
 import com.api.testes.application.user.UserRepository;
 import com.api.testes.application.user.UserService;
 import com.api.testes.domain.user.User;
+import com.api.testes.exceptions.BusinessException;
 import com.api.testes.exceptions.NotFoundException;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
-public class UserServiceImpl implements UserService{
+@Slf4j
+public class UserServiceImpl implements UserService {
 
     private UserRepository repository;
 
@@ -22,29 +26,48 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public User saveUser(User user) throws Exception{
-        return repository.save(user);
+    public User saveUser(User user) throws BusinessException {
+        log.info("Salvando usuario: " + user);
+        try {
+            return repository.save(user);
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao cadastrar usuario");
+        }
     }
 
     @Override
-    public List<User> findAll() throws Exception{
-        return repository.findAll();
+    public List<User> findAll() throws NotFoundException {
+        log.info("Buscando todos os usuarios...");
+        List<User> users = repository.findAll();
+        if (!users.isEmpty()) {
+            throw new NotFoundException("Nenhum usuario encontrado");
+        }
+
+        return users;
     }
 
     @Override
     public User findById(Integer id) throws Exception {
+        log.info("Buscando usuario com id: " + id);
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new NotFoundException("Usuario nao encontrado"));
     }
 
     @Override
-    public User update(User user) throws Exception {
-        findById(user.getId());
-        return repository.save(user);
+    public User update(User user) throws BusinessException {
+        log.info("Atualizando usuario...");
+        try {
+            findById(user.getId());
+            return repository.save(user);
+        } catch (Exception e) {
+            log.info("Erro ao atualizar usuario");
+            throw new BusinessException("Erro ao atualizar usuario");
+        }
     }
 
     @Override
     public void delete(Integer id) throws Exception {
+        log.info("Deletando usuario com id: " + id);
         findById(id);
         repository.deleteById(id);
     }
--- src/main/java/com/api/testes/exceptions/ExceptionHandler.java
+++ src/main/java/com/api/testes/exceptions/ExceptionHandler.java
@@ -17,4 +17,16 @@ public class ExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
+    @org.springframework.web.bind.annotation.ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorDetail> badRequestException(BadRequestException exception, HttpServletRequest request){
+        ErrorDetail error = new ErrorDetail(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorDetail> businessException(BusinessException exception, HttpServletRequest request){
+        ErrorDetail error = new ErrorDetail(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
 }
