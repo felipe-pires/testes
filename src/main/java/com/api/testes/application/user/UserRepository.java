656070032711d9de64372907453274e2da635488
Felipe dos Santos <felipedossantospires01@gmail.com>
2023-01-23 11:59:33 -0300

    fix: email exception

--- src/main/java/com/api/testes/application/user/UserRepository.java
+++ src/main/java/com/api/testes/application/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.api.testes.application.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.api.testes.domain.user.User;
@@ -8,4 +11,7 @@ import com.api.testes.domain.user.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
     
+    @Query(nativeQuery = true, value = "select * from users where email = :email")
+    Optional<User> findByEmail(String email);
+
 }
--- src/main/java/com/api/testes/application/user/impl/UserServiceImpl.java
+++ src/main/java/com/api/testes/application/user/impl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import com.api.testes.application.user.UserService;
 import com.api.testes.domain.dto.UserDTO;
 import com.api.testes.domain.user.User;
 import com.api.testes.exceptions.BusinessException;
+import com.api.testes.exceptions.DataIntegratyViolationException;
 import com.api.testes.exceptions.NotFoundException;
 
 import lombok.extern.slf4j.Slf4j;
@@ -30,9 +31,14 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User saveUser(UserDTO userDto) throws BusinessException {
+    public User saveUser(UserDTO userDto) throws BusinessException, DataIntegratyViolationException {
         log.info("Salvando usuario: " + userDto.getName());
         User user = mapper.map(userDto, User.class);
+
+        if (repository.findByEmail(user.getEmail()).isPresent()) {
+            throw new DataIntegratyViolationException("email ja existe na base de dados");
+        }
+
         try {
             return repository.save(user);
         } catch (Exception e) {
@@ -48,7 +54,7 @@ public class UserServiceImpl implements UserService {
         if (users.isEmpty()) {
             throw new NotFoundException("Nenhum usuario encontrado");
         }
-       
+
         return users;
     }
 
@@ -76,5 +82,12 @@ public class UserServiceImpl implements UserService {
         log.info("Deletando usuario com id: " + id);
         findById(id);
         repository.deleteById(id);
+    }
+
+    @Override
+    public User findByEmail(String email) throws Exception {
+        log.info("Buscando usuario com email: " + email);
+        Optional<User> user = repository.findByEmail(email);
+        return user.orElseThrow(() -> new NotFoundException("Usuario nao encontrado"));
     }
 }
