a71551bb13566ce33c83519d548089aaee521c19
Felipe dos Santos <felipedossantospires01@gmail.com>
2023-01-30 11:45:53 -0300

    test

--- src/test/java/com/api/testes/TestesApplicationTests.java
+++ src/test/java/com/api/testes/TestesApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TestesApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		TestesApplication.main(new String[] {});
 	}
 
 }
